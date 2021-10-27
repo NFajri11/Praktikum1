71d7bc88dda9d46fd962ed000348baa693503a30
Your Name <you@example.com>
2021-10-27 14:33:55 +0700

    Memambahkan file latihan1.py

---
+++ latihan1.py
@@ -0,0 +1,20 @@
+print("Hello")
+print("Saya sedang belajar pyton")
+
+a = 8
+b = 6
+print ("variable a=",a)
+print ("variable b=",b)
+print ("hasil penjumlahan a+b=",a+b)
+
+a=input("masukan nilai a:")
+b=input("masukan nilai b:")
+print("variable a=",a)
+print("variable b=",b)
+print("hasil penggabungan {1}&{0}=%s".format(a,b) %(a+b))
+
+#konvensi nilai variable
+a=int(input("masukan nilai a:"))
+b=int(input("masukan nilai b:"))
+print("hasil penjumlahan {1}+{0}=%d". format(a,b) %(a+b))
+print("hasil pembagiann {1}/{0}=%f". format (a,b) %(a/b))
